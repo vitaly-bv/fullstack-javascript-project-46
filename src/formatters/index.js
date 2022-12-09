010fb53d03d0bf11c5bf5e3b3af1008abf71aed9
vitaly-bv <doomedmess@gmail.com>
2022-12-09 07:15:47 +0100

    make stylish

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,10 @@
-import makeStylish from './stylish.js';
+import stylish from './stylish.js';
 import plain from './plain.js';
 
 const chooseFormat = (data, format) => {
   switch (format) {
-    case 'makeStylish':
-      return makeStylish(data);
+    case 'stylish':
+      return stylish(data);
     case 'plain':
       return plain(data);
     case 'json':
